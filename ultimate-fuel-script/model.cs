152f4b6c09ca4ec3ff5060e1b88b31326ac0b314
pedro2555 <prodrigues1990@gmail.com>
2014-04-23 14:47:52 +0100

    Moved player location aware logic to model component

--- ultimate-fuel-script/model.cs
+++ ultimate-fuel-script/model.cs
@@ -47,6 +47,16 @@ namespace ultimate_fuel_script
 
         void model_Tick(object sender, EventArgs e)
         {
+            // Change CurrentFuelStation according to player location and moving method
+            if (Player.Character.isInVehicle() && Player.Character.CurrentVehicle.GetPedOnSeat(VehicleSeat.Driver) == Player.Character && Player.Character.CurrentVehicle.Speed < 1.5f)
+            {
+                // This basically tests if the player is at the closest station found.
+                model.CurrentFuelStation = FuelStation.IsAtStation(
+                    FuelStation.GetNearestStation(
+                        Player.Character.Position,
+                        FuelStation.GetStationTypeFromVehicle(Player.Character.CurrentVehicle)),
+                    Player.Character.Position);
+            }
         }
 
         #region Methods
