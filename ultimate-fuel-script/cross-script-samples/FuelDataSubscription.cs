7459d40be33bda53570a50d14e47eeffd9f3df92
pedro2555 <prodrigues1990@gmail.com>
2014-04-29 10:25:31 +0100

    Added FuelDataSubscription samples

---
+++ ultimate-fuel-script/cross-script-samples/FuelDataSubscription.cs
@@ -0,0 +1,23 @@
+﻿using GTA;
+// Required to access FuelData class
+using ultimate_fuel_script;
+
+class FuelDataSubscription : Script
+{
+    public FuelDataSubscription()
+    {
+        // Bind to subscription update notifications
+        BindScriptCommand("FuelDataSubscription_update", new ScriptCommandDelegate(FuelDataSubscription_handler));
+        // Request a subscription
+        SendScriptCommand("775df3cb-41c0-45f7-bd8f-d989853c838b", "FuelDataSubscription", null);
+    }
+
+    /// <summary>
+    /// Handle notification updates
+    /// </summary>
+    private void FuelDataSubscription_handler(GTA.Script sender, GTA.ObjectCollection Parameter)
+    {
+        // Assign fuel data object
+        FuelData update = (FuelData)Parameter[0];
+    }
+}
---
+++ ultimate-fuel-script/cross-script-samples/FuelDataSubscription.vb
@@ -0,0 +1,21 @@
+﻿Imports GTA
+' Required to access FuelData class
+Imports ultimate_fuel_script
+
+Class FuelDataSubscription
+    Inherits Script
+    Public Sub New()
+        ' Bind to subscription update notifications
+        BindScriptCommand("FuelDataSubscription_update", New ScriptCommandDelegate(AddressOf FuelDataSubscription_handler))
+        ' Request a subscription
+        SendScriptCommand("775df3cb-41c0-45f7-bd8f-d989853c838b", "FuelDataSubscription", Nothing)
+    End Sub
+
+    ''' <summary>
+    ''' Handle notification updates
+    ''' </summary>
+    Private Sub FuelDataSubscription_handler(sender As GTA.Script, Parameter As GTA.ObjectCollection)
+        ' Assign fuel data object
+        Dim update As FuelData = DirectCast(Parameter(0), FuelData)
+    End Sub
+End Class
